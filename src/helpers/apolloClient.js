1f86ccbf038d7f07276d250e5ce4836cdc060ad3
Gustavo Marquez <gustavomm19@hotmail.com>
2020-05-30 10:33:08 -0400

    auth user

--- src/helpers/apolloClient.js
+++ src/helpers/apolloClient.js
@@ -2,12 +2,11 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
-const endpoint = "https://examplee/graphql";
+//const endpoint = "https://examplee/graphql";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: endpoint,
-  //uri: "http://190.9.41.85:4000/graphql"
+  uri: "https://porta-api.herokuapp.com/graphql"
 });
 
 const client = new ApolloClient({
