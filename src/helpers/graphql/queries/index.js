64af1b5d4276148e24510f1facedb699d2e446e3
spectrox0 <alejanvelazco2008@hotmail.com>
2020-06-02 16:53:40 -0400

    A

--- src/helpers/graphql/queries/index.js
+++ src/helpers/graphql/queries/index.js
@@ -1,22 +1,20 @@
 import gql from "graphql-tag";
 export const LOGIN_USER = gql`
-   query ($mail: String! , password: String!) {
-        userLogin(mail: $mail, password: $password){
-        userId
-        token
-        tokenExpiration
+  query($mail: String!, $password: String!) {
+    userLogin(mail: $mail, password: $password) {
+      userId
+      token
+      tokenExpiration
     }
-   }
- 
+  }
 `;
 
 export const ADMIN_LOGIN = gql`
-   query ($mail: String! , password: String!) {
-        adminLogin(mail: $mail, password: $password){
-            adminId
-            token
-            tokenExpiration
+  query($mail: String!, $password: String!) {
+    adminLogin(mail: $mail, password: $password) {
+      adminId
+      token
+      tokenExpiration
     }
-   }
- 
+  }
 `;
