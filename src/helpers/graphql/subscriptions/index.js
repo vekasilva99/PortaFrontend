c3a3677cc7d3724dc6f4d9c054bdf4c6819e536f
Veka Silva <vekasilva99@gmail.com>
2020-07-02 12:31:23 -0400

    auxilio

--- src/helpers/graphql/subscriptions/index.js
+++ src/helpers/graphql/subscriptions/index.js
@@ -70,7 +70,7 @@ export const NEW_MESSAGE = gql`
 `;
 
 export const ORDER_UPDATE = gql`
-  subscription($userId: ID!) {
+  subscription($userId: String!) {
     orderUpdate(userId: $userId) {
       _id
       user {
