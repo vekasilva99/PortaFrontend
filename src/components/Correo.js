e4c333fa6ce2848b932082890f75b8746b29e6da
ValeriaTrotta <valeria.trotta@correo.unimet.edu.ve>
2020-07-08 03:53:53 -0400

    email

--- src/components/Correo.js
+++ src/components/Correo.js
@@ -12,20 +12,35 @@ import { useSelector, useDispatch } from "react-redux";
 import Spinner from "./Spinner";
 import { NOTIFICATION_ADDED_SUSCRIPTION } from "../helpers/graphql/subscriptions/index";
 import { ACCEPT_ORDER } from "../helpers/graphql/mutations/index";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default function Correo() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <StyledCorreo>
       <form className="forma">
         <label className="lab">
           Subject
-          <input className="sub" type="text" name="subject" />
+          <input className="sub" type="text" />
         </label>
         <label className="lab">
           Content
-          <input className="cont" type="text" name="content" />
+          <input className="cont" type="text" />
         </label>
-        <input type="submit" value="Submit" />
+        <input className="but" type="submit" value="Submit" />
+        <div className="check">
+          <h4>Correo recibido</h4>
+          <Checkbox
+            disabled
+            checked
+            inputProps={{ "aria-label": "disabled checked checkbox" }}
+          />
+        </div>
       </form>
     </StyledCorreo>
   );
@@ -41,14 +56,59 @@ const StyledCorreo = styled.nav`
   .forma {
     display: inline-grid;
   }
+
+  .check {
+    display: flex;
+    /* h4 {
+      margin-top: 10px;
+    } */
+  }
+
+  .check {
+    margin-top: 20px;
+    align-items: center;
+  }
+
   .lab {
     display: inline-grid;
   }
+  .but {
+    border: solid 2px #00507a;
+    color: white;
+    padding: 10px;
+    font-size: 15px;
+    width: 200px;
+    display: flex;
+    font-weight: 600;
+    cursor: pointer;
+    background: #00507a;
+    border-radius: 500px;
+    transition: all ease-in-out 0.3s;
+    justify-content: center;
+    &:hover {
+      opacity: 0.8;
+      background: #00507a;
+      color: white;
+      border-color: #00507a;
+    }
+    &:focus {
+      opacity: 0.8;
+      outline: none;
+      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    }
+  }
 
   @media only screen and (min-width: 970px) {
-    /* .forma {
+    .sub {
       width: 40rem;
-    } */
+    }
+    .cont {
+      width: 40rem;
+      height: 300px;
+    }
+    .but {
+      width: 200px;
+    }
   }
 
   @media only screen and (max-width: 969px) and (min-width: 735px) {
