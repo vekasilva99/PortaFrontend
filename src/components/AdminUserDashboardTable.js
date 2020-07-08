d685e3d2f497d2d2f74dbbd99e38df13c4d43e59
Gustavo Marquez <gustavomm19@hotmail.com>
2020-07-08 02:06:24 -0400

    new queries

--- src/components/AdminUserDashboardTable.js
+++ src/components/AdminUserDashboardTable.js
@@ -2,19 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink, withRouter } from "react-router-dom";
 import { BsCircleFill } from "react-icons/bs";
-import {
-  GET_USERS,
-  GET_REPARTIDORES,
-  NEW_USERS,
-  NEW_REPARTIDORES,
-} from "../helpers/graphql/queries";
+import { NEW_USERS } from "../helpers/graphql/queries";
 import { useQuery } from "@apollo/react-hooks";
 
 export default function AdminTable(props) {
-  const [users, setUsers] = React.useState([
-    { id: 1, name: "Wasif", age: 21, email: "wasif@email.com" },
-    { id: 2, name: "Ali", age: 19, email: "ali@email.com" },
-  ]);
 
   //Nuevos usuarios
   const { data: dataNU, error: errorNU, loading: loadingNU } = useQuery(
@@ -23,9 +14,6 @@ export default function AdminTable(props) {
 
   if (loadingNU) return "Loading...";
   if (errorNU) return `Error! ${errorNU.message}`;
-
-  
-    console.log(new Date().toISOString());
 
   return (
     <StyledTable>
